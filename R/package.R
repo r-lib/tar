3bdabd77b223af23b1e6d190cec51d5ccec058d7
Gábor Csárdi <csardi.gabor@gmail.com>
2017-12-16 11:03:09 +0000

    Fix up Mason

--- R/package.R
+++ R/package.R
@@ -2,8 +2,9 @@
 #' R's Internal 'tar' Implementation as a Package
 #'
 #' There have been various issues with R's builtin 'tar' implementation
-  recently. This packages contains a version that fixes several of these
-  issues, so we can have a stable implementation that works with any R version.
+#' recently. This packages contains a version that fixes several of these
+#' issues, so we can have a stable implementation that works with any R
+#' version.
 #'
 #' @docType package
 #' @name tar
